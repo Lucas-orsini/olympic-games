e97ad80f4e73b7fc40166b79675f0708c4b450d1
LucasO <=>
2022-07-21 22:48:37 +0200

    fix

--- bot-discord/bot.js
+++ bot-discord/bot.js
@@ -1,8 +1,8 @@
 require('dotenv').config()
 const Discord = require('discord.js')
-const { Client, GatewayIntentBits } = require('discord.js');
+const { GatewayIntentBits } = require('discord.js');
 const MysqlConnector = require('./src/MySqlConnector');
-const TOKEN = 'OTkxNjA2NDg2Mjk4MDE3ODMy.GftPK8.KJNvYbk1raKgCfQtNS4DGYZPRt0X64Dsfv8Q_Y';
+const TOKEN = 'OTkxNjA2NDg2Mjk4MDE3ODMy.GYd8yH.n3Ol0S11c1jqM7Rn-jL--A3om071h_QN9dcK9Q';
 const commandLoader = require("./commandLoader")
 MysqlConnector.connect();
 
@@ -10,30 +10,30 @@ MysqlConnector.connect();
 
 const bot = new Discord.Client({
     intents: [   GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages]
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMembers]
 })
 commandLoader.load(bot);
-bot.on('messageCreate', async(message) => {
- 
-
-
-    if (message.content.startsWith("!")) {
+bot.on('create', async(message) => {
+     if (message.content.startsWith("!")){
+        console.log('yupi')
         let words = message.content.split(' ');
-        const commandName = words.shift().slice(1);
-        const arguments = words;
+        console.log(words)
+        const nameCommand = words.shift().slice(1);
+        console.log(nameCommand)
+        const args = words;
+        console.log(args)
 
-        if (bot.commands.has(commandName)) {
-        
-            bot.commands.get(commandName).run(bot, message, arguments);
+        if (bot.commands.has(nameCommand)) {
+            
+            bot.commands.get(nameCommand).run(bot, message, args);
         } else {
-           
+     
             await message.delete();
-            await message.channel.send(`The ${commandName} command does not exist.`);
+            await message.channel.send(`The ${nameCommand} does not exist.`);
         }
 
     }
-
-
 })
             bot.login(TOKEN)
 
--- bot-discord/commands/sport.js
+++ bot-discord/commands/sport.js
@@ -9,11 +9,12 @@ const mySqlConnector = require('../src/mySqlConnector');
 module.exports.run = async(client, message, arguments) => {
 
     mySqlConnector.connect();
+    console.log('test')
     let content = message.content;
 
     let classement = await mySqlConnector.executeQuery("SELECT * FROM resultat WHERE sport = " + content);
-    print(classement)
     await message.channel.send(`Podium Premier : ${classement[0].premier} / Deuxieme : ${classement[0].deuxieme} / Troisieme :${classement[0].deuxieme} `);
+   
 };
 
 module.exports.name = 'sport'
